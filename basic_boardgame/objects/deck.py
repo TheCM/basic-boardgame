5d732c96495ddaf6f22a30a59c66858bc9fd9007
Remigiusz Z <remigiusz.zamiara@gmail.com>
2022-01-09 02:28:19 +0100

    Create simple Deck class with some basic methods

---
+++ basic_boardgame/objects/deck.py
@@ -0,0 +1,29 @@
+from random import shuffle
+from typing import List
+
+from basic_boardgame.objects.card import Card
+
+
+class Deck:
+    def __init__(self, cards: List[Card]):
+        self.cards = cards
+
+    def shuffle(self):
+        shuffle(self.cards)
+
+    def get_top_card(self):
+        return self.cards.pop(0)
+
+    def get_bottom_card(self):
+        return self.cards.pop(-1)
+
+    def get_all_cards(self):
+        giving_away_cards = self.cards[:]
+        self.cards.clear()
+        return giving_away_cards
+
+    def place_card_at_bottom(self, card: Card):
+        self.cards.append(card)
+
+    def place_cards_at_bottom(self, cards: List[Card]):
+        self.cards += cards
