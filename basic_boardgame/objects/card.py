6738a0d8867943cfa6ae71f23ca77ab6e5c6ca52
Remigiusz Z <remigiusz.zamiara@gmail.com>
2022-01-15 02:01:36 +0100

    Add advanced card class

--- basic_boardgame/objects/card.py
+++ basic_boardgame/objects/card.py
@@ -4,3 +4,15 @@ class Card:
 
     def __str__(self):
         return self.name
+
+
+class AdvancedCard(Card):
+    def __init__(self, name: str, description: str):
+        super().__init__(name)
+        self.description = description
+
+    def __str__(self):
+        return f"{self.name}\n {self.description}"
+
+    def show_description(self):
+        return self.description
